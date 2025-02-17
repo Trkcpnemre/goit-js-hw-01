e7e6ed0a611e280528eb96bf2c58cde0bf0a2b57
emre_cimen <fatsaliemre52@gmail.com>
2025-02-17 20:17:41 +0100

    change

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,13 @@
 function getElementWidth(content, padding, border) {
-    return (parseFloat(padding) + parseFloat(border)) * 2 + parseFloat(content);
-  }
-  
-  console.log(getElementWidth("50px", "8px", "4px"));
+    
+    const contentWidth = parseFloat(content);
+    const paddingValue = parseFloat(padding);
+    const borderValue = parseFloat(border);
+
+    
+    return contentWidth + paddingValue * 2 +  borderValue * 2;
+}
+
+
+console.log(getElementWidth("50px", "8px", "4px")); 
+
